ace0716c1c0ec7e1d920ffeb919ec9ff7407dd92
Leandro Richard Giehl <119633959+LeandroRichardGiehl@users.noreply.github.com>
2023-10-18 21:17:45 -0300

    commit

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function login() {
     //verifica se o email e senha corresponde a um usuário
     const user = users.find(u => u.email === email && u.senha === senha);
 
-    if(user){
+    users.push({email, senha})
+
+    if(email.trim()=== '' || senha.trim() === ''){
+        alert('Por favor faça o cadastro primeiro')
+    }
+    else if(user){
         document.getElementById('result').innerHTML = 'Login bem-sucedido';
     } else {
         document.getElementById('result').innerHTML = 'Login falhou';
@@ -18,6 +23,7 @@ function login() {
 
 function register() {
     //variáveis que salvam o prompt do HTML
+    const name = document.getElementById('register-name').value;
     const email = document.getElementById('register-email').value;
     const senha = document.getElementById('register-senha').value;
 
@@ -25,13 +31,13 @@ function register() {
     const users = JSON.parse(localStorage.getItem('users')) || [];
 
     //registra o email e a senha no banco de dados
-    users.push({email, senha})
+    users.push({name, email, senha})
 
     //salva a lista atualizada de usuários no localStorage
     localStorage.setItem('users', JSON.stringify(users))
-
-    if (email.trim() === '' || senha.trim() === '') {
-        alert('Por favor, preencha todos os campos de cadastro.');
+    
+    if (name.trim() === '' || email.trim() === '' || senha.trim() === '') {
+        alert('Por favor, preencha todos os dados cadastrais.');
         return;
     }else{
         document.getElementById('result').innerHTML = 'Cadastro feito com sucesso';
